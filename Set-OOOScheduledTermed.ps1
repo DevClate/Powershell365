129a3b271458fcd05341dcad536b786394864a41
DevClate <107187714+DevClate@users.noreply.github.com>
2022-08-08 05:20:10 -0400

    Added Parameters

---
+++ Set-OOOScheduledTermed.ps1
@@ -0,0 +1,31 @@
+#Sets out of office for internal and external
+
+[CmdletBinding()]
+    param (
+    [Parameter(Mandatory)]
+    [string]$UserPrincipalName,
+
+    [Parameter(Mandatory,HelpMessage='Enter the user name for OOO!')]
+    [string]$UserName,
+
+    [Parameter(Mandatory,HelpMessage='Enter start time, ex 04/14/2022 21:30:00')]
+    [string]$StartTime,
+
+    [Parameter(Mandatory,HelpMessage='Enter end time, ex 04/14/2022 21:30:00')]
+    [string]$EndTime,
+
+    [Parameter(Mandatory,HelpMessage='Enter Manager''s Name')]
+    [string]$ManagerName,
+
+    [Parameter(Mandatory,HelpMessage='Enter Manager''s Email')]
+    [string]$ManagerEmail
+    )
+
+    $ExternalMessage = "Thank you for your email. Please note that this email address is no longer active. Please redirect your email to $ManagerName at $ManagerEmail."
+    $InternalMessage = "Thank you for your email. Please note that this email address is no longer active. Please redirect your email to $ManagerName at $ManagerEmail."
+
+#Connect to Exchange Online
+Connect-ExchangeOnline -UserPrincipalName $UserPrincipalName
+
+#Sets out of office for internal and external
+Set-MailboxAutoReplyConfiguration -Identity $UserName -AutoReplyState Scheduled -StartTime $StartTime -EndTime $EndTime -InternalMessage $InternalMessage -ExternalMessage $ExternalMessage
---
+++ Set-OOOTermed.ps1
@@ -0,0 +1,24 @@
+#Sets out of office for internal and external
+[CmdletBinding()]
+    param (
+    [Parameter(Mandatory)]
+    [string]$UserPrincipalName,
+
+    [Parameter(Mandatory,HelpMessage='Enter the user name for OOO!')]
+    [string]$UserName,
+
+    [Parameter(Mandatory,HelpMessage='Enter Manager''s Name')]
+    [string]$ManagerName,
+
+    [Parameter(Mandatory,HelpMessage='Enter Manager''s Email')]
+    [string]$ManagerEmail
+    )
+
+    $ExternalMessage = "Thank you for your email. Please note that this email address is no longer active. Please redirect your email to $ManagerName at $ManagerEmail."
+    $InternalMessage = "Thank you for your email. Please note that this email address is no longer active. Please redirect your email to $ManagerName at $ManagerEmail."
+
+#Connect to Exchange Online
+Connect-ExchangeOnline -UserPrincipalName $UserPrincipalName
+
+#Sets out of office for internal and external
+Set-MailboxAutoReplyConfiguration -Identity $UserName -AutoReplyState Enabled -InternalMessage $InternalMessage -ExternalMessage $ExternalMessage
