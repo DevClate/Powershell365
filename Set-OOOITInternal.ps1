0e21053cc661c318a58d9a8ff4c9df6a4e75464c
DevClate <107187714+DevClate@users.noreply.github.com>
2022-08-24 07:40:09 -0400

    Update Set-OOOITInternal.ps1

--- Set-OOOITInternal.ps1
+++ Set-OOOITInternal.ps1
@@ -1,6 +1,6 @@
 #Created 4/14/2022
 #Sets a scheduled out of office for interal users
-#Email body doesn't pull date from Start/End time yet, needs to be manually inputted for now, you could setup another paramter without time and just don't though
+#Email body doesn't pull date from Start/End time yet, needs to be manually inputted for now
 #21:30:00 is 5:30PM Eastern Time
 
 [CmdletBinding()]
@@ -8,7 +8,7 @@ param (
     [Parameter(Mandatory)]
     [string]
     $UserPrincipalName,
-    [Parameter(Mandatory,HelpMessage='Enter the user name for OOO!')]
+    [Parameter(Mandatory,HelpMessage='Enter the user name for OOO')]
     [string]
     $UserName,
     [Parameter(Mandatory,HelpMessage='Enter start time, ex 04/14/2022 21:30:00')]
@@ -26,7 +26,7 @@ param (
     [Parameter(Mandatory,HelpMessage='Enter Ext of Help')]
     [string]
     $ExtHelp,
-    $InternalMessage = "Thank you for your email. I will be out of the office Friday, 4/15 with a return date of Monday, 4/18. If your email is of urgent nature, please contact the help desk at extension $ExtHelp. If you need immediate assistance, please contact $ManagerName at extension $Ext. Thank you."
+    $InternalMessage = "Thank you for your email. I will be out of the office Wednesday, 8/24 with a return date of Friday, 8/26. If your email is of urgent nature, please contact the help desk at extension $ExtHelp. If you need immediate assistance, please contact $ManagerName at extension $Ext. Thank you."
 
 )
 
